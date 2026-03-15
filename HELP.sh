5b7cb95e60842966f6b7811a7169f191f6eaaf98
devide3301 <fantemti@gmail.com>
2026-03-15 06:25:37 -0300

    Update HELP.sh

--- HELP.sh
+++ HELP.sh
@@ -1,1 +1,9 @@
-3
+#!/bin/bash
+
+echo cucha tenés q hacer estos pasos ok??
+echo ---
+echo Primero hace: 'su -'
+echo después hace: 'usermod -aG sudo {user}'
+echo reemplaza {user} por el user te podes fijar con un whoami
+echo después hace reboot y debería estar
+echo podes comprobar con sudo whoami, si salió bien te va a tirar root
